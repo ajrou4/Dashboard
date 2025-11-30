d4bca01c6c183a9a5932bd54ed31974e8033cbe3
ajrou4 <mohamed1999ajro@gmail.com>
2025-11-30 22:01:37 +0100

    Add ClerkProvider to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import { ClerkProvider } from "@clerk/nextjs"; // <--- Import this
+import { ClerkProvider } from "@clerk/nextjs"; // <--- 1. Import this
 import "./globals.css";
 
 const geistSans = Geist({
@@ -24,7 +24,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider> {/* <--- Wrap everything in this */}
+    // 2. Wrap the HTML tag with ClerkProvider
+    <ClerkProvider>
       <html lang="en">
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
